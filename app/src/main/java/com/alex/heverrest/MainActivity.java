754ba74ad9992d5fd6a3291dedf89fe85b4969ff
alex-ilgayev <alex.ilgayev@gmail.com>
2017-01-27 13:04:28 +0200

    first commit

---
+++ app/src/main/java/com/alex/heverrest/MainActivity.java
@@ -0,0 +1,155 @@
+package com.alex.heverrest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.alex.heverrest.Model.Restaurant;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView mLvRests;
+    List<Restaurant> mRests = new LinkedList<>();
+    RestAdapter mAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLvRests = (ListView) findViewById(R.id.lvRests);
+
+        populateRestaurants(mRests);
+
+        mAdapter = new RestAdapter(this, mRests);
+
+        mLvRests.setAdapter(mAdapter);
+
+        mLvRests.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Restaurant r = mRests.get(position);
+                //Uri uri = Uri.parse("geo:0,0?q=" + Uri.encode(r.address));
+                Uri uri = Uri.parse("geo:0,0?q=" + r.lat + "," + r.lng);
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                i.setPackage("com.google.android.apps.maps");
+                startActivity(i);
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    public void populateRestaurants(List rests) {
+        rests.add(Restaurant.r1);
+        rests.add(Restaurant.r2);
+        rests.add(Restaurant.r3);
+        rests.add(Restaurant.r4);
+        rests.add(Restaurant.r5);
+    }
+
+    class RestAdapter extends ArrayAdapter<Restaurant> {
+
+
+        public RestAdapter(Context context, List<Restaurant> objects) {
+            super(context, 0, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Restaurant rest = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_rest, parent, false);
+            }
+            // Lookup view for data population
+            TextView tvName = (TextView) convertView.findViewById(R.id.tvRestName);
+            TextView tvAddress = (TextView) convertView.findViewById(R.id.tvRestAddress);
+            TextView tvRestSubType = (TextView) convertView.findViewById(R.id.tvRestSubType);
+            ImageView ivPic = (ImageView) convertView.findViewById(R.id.ivPic);
+
+            // Populate the data into the template view using the data object
+            tvName.setText(rest.name);
+            tvAddress.setText(rest.address);
+            ivPic.setImageResource(rest.picRes);
+
+            int i = 0;
+            String s = "";
+            for(Restaurant.RestSubType type: rest.subType) {
+                if(i != 0)
+                    s += " | ";
+                s += type;
+                i++;
+            }
+            tvRestSubType.setText(s);
+
+            // Return the completed view to render on screen
+            return convertView;
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/alex/heverrest/Model/Restaurant.java
@@ -0,0 +1,120 @@
+package com.alex.heverrest.Model;
+
+import com.alex.heverrest.R;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 1/20/2017.
+ */
+
+public class Restaurant {
+
+    public enum RestType {
+        Caffee,
+        Restaurant,
+        Bar,
+        Icecream,
+        Gourmet,
+        Pastry,
+        Bakery,
+    }
+
+    public enum RestSubType {
+        Italian("איטלקי"),
+        Asian("אסייתי"),
+        Mexican("מקסיקני"),
+        Sushi("סושי"),
+        French("צרפתי"),
+        Vegan("צמחוני-טבעוני"),
+        Sweets("מתוקים-קינוחים"),
+        Meat("בשרים"),
+        Coffee("בית קפה"),
+        Sandwiches("כריכים-סלטים"),
+        Dairy("חלבי"),
+        Fish("דגים"),
+        Bar("בר-פאב"),
+        Mediterranean("ים תיכוני"),
+        MiddleEastern("מזרחי"),;
+
+        private String type;
+
+        RestSubType(String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return this.type;
+        }
+    }
+
+    public int id;
+    public String name;
+    public RestType[] type;
+    public RestSubType[] subType;
+    public String address;
+    public double lat;
+    public double lng;
+    public int openEmza;
+    public int closeEmza;
+    public int openShishi;
+    public int closeShishi;
+    public int openShabat;
+    public int closeShabar;
+    public String phoneNumber;
+    public int picRes;
+
+    public Restaurant(int id, String name, RestType[] restTypes, RestSubType[] restSubTypes,
+                      String address, double lat, double lng, int picRes) {
+        this.id = id;
+        this.name = name;
+        this.type = restTypes;
+        this.subType = restSubTypes;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+        this.picRes = picRes;
+    }
+
+    public static Restaurant r1 = new Restaurant(1,"BEIR GARTEN",
+            new RestType[]{RestType.Bar},
+            new RestSubType[]{RestSubType.Bar},
+            "תל אביב - יפו, התערוכה 3 ביתן 16",
+            32.096483,
+            34.773274,
+            R.drawable.r1);
+
+    public static Restaurant r2 = new Restaurant(2,"Black",
+            new RestType[]{RestType.Restaurant},
+            new RestSubType[]{RestSubType.Meat},
+            "תל אביב - יפו, הברזל 23",
+            32.108941,
+            34.839759,
+            R.drawable.r2);
+
+    public static Restaurant r3 = new Restaurant(2,"Bread Story",
+            new RestType[]{RestType.Caffee},
+            new RestSubType[]{RestSubType.Coffee},
+            "תל אביב - יפו, דיזינגוף 88",
+            32.076959,
+            34.774479,
+            R.drawable.r3);
+
+    public static Restaurant r4 = new Restaurant(2,"BROWN",
+            new RestType[]{RestType.Caffee, RestType.Restaurant},
+            new RestSubType[]{RestSubType.Italian, RestSubType.Dairy, RestSubType.Sweets},
+            "תל אביב - יפו, ניסים אלוני 10",
+            32.088866,
+            34.797339,
+            R.drawable.r4);
+
+    public static Restaurant r5 = new Restaurant(2,"Chop Chop",
+            new RestType[]{RestType.Restaurant},
+            new RestSubType[]{RestSubType.Asian},
+            "תל אביב - יפו, אבן גבירול 20",
+            32.073648,
+            34.781894,
+            R.drawable.r5);
+}
+
